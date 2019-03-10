eee300c0dc2d7801b18ad3c031ba0432d7483740
Johan <diels.johan@gmail.com>
2019-03-10 17:31:25 +0100

    refactor folder structure

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'dataplatformdump.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'dataplatformdump.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
